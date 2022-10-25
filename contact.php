2bfb31b0b00edeab5fd6295b849b3e2129ce5907
luciole12 <lucie.canevarolo@gmail.com>
2022-10-25 11:41:26 +0200

    css menu et footer

---
+++ contact.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Contact</title>
+    
+        <!-- Favicons -->
+      <link href="img/favicon.png" rel="icon">
+      <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+    
+      <!-- Main Stylesheet File -->
+      <link href="style.css" rel="stylesheet">
+      </head>
+
+<body>
+
+    <header>
+        <?php
+        include('menu.html');
+        ?>
+      </header>
+<!--
+    <div>
+        <img src="img/artiste/banniere.png">
+    </div>-->
+        <section>
+            <div class="container">
+    
+                <section class="contact">
+    
+                    <div class="container-contact">
+                        <div class="section-contact">
+                            <h2>CONTACT</h2>
+                            <p>Adresse mail: artinkevent@gmail.com<br>
+                            Téléphone: 06.05.23.63.78<br>
+                            Dates:<br><br>
+                            - Vendredi 30 Juin<br>
+                            - Samedi 01 Juillet<br>
+                            - Dimanche 02 Juillet<br><br>
+                            Lieux: Place du Breuil, 43 000, Le Puy-en-Velay
+                            </p>
+                        </div>
+    
+                        <div class="row map">
+                            <div class="col-md-12 map">
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2819.1568918985054!2d3.8838153!3d45.042038!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f5fa53f56b82b3%3A0xdcc562821317984c!2sPl.%20du%20Breuil%2C%2043000%20Le%20Puy-en-Velay!5e0!3m2!1sfr!2sfr!4v1666598023384!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </section>
+
+        <footer>
+        <?php
+        include('footer.html');
+        ?>
+      </footer>
+
+    </body>   
+</html>
