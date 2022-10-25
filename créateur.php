ce28f8b17866358e8eccf6ee9a182b3eaa533b8b
CdPValentin <valromeas43@gmail.com>
2022-10-26 00:17:59 +0200

    Update créateur.php

--- créateur.php
+++ créateur.php
@@ -6,24 +6,24 @@
         <div>
             <h2>Les tatoueurs</h2>
             <h1>France</h1>
-            <img src="../img/dodie.jpg" height="600px">
-            <img src="../img/sandry.jpg" height="600px">
+            <img src=".img/artistes/dodie.jpg" height="600px">
+            <img src="img/artistes/sandry.jpg" height="600px">
             <h1>U.K</h1>
-            <img src="../img/dani.jpg" height="600px">
-            <img src="../img/maxime.jpg" height="600px">
+            <img src="img/artistes/dani.jpg" height="600px">
+            <img src="img/artistes/maxime.jpg" height="600px">
             <h1>Belges</h1>
-            <img src="../img/jpmottin.jpg" height="600px">
-            <img src="../img/inal.jpg" height="600px">
+            <img src="img/artistes/jpmottin.jpg" height="600px">
+            <img src="img/artistes/inal.jpg" height="600px">
             <h1>Allemand</h1>
-            <img src="../img/dotstolines.jpg" height="600px">
-            <img src="../img/peter.jpg" height="600px">
+            <img src="img/artistes/dotstolines.jpg" height="600px">
+            <img src="img/artistes/peter.jpg" height="600px">
 
             <h2>Les perceurs</h2>
-            <img src="../img/maviedereve.jpg" height="600px">
-            <img src="../img/elena.jpg" height="600px">
+            <img src="img/artistes/maviedereve.jpg" height="600px">
+            <img src="img/artistes/elena.jpg" height="600px">
 
             <h2>La dessinatrice</h2>
-            <img src="../img/lucile.jpg" height="600px">
+            <img src="img/artistes/lucile.jpg" height="600px">
         </div>
     </body>
     <fooder>
